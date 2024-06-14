7b8530699d20a7e875fe262084abe2155d1782e7
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-06-14 22:00:23 +0700

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -1,98 +1,316 @@
-getgenv().lobbyFPS = 5
-getgenv().inGameFPS = 4
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local getInventoryRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("GetInventory")
+local HttpService = game:GetService("HttpService")
+local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local Mouse = player:GetMouse()
+local promptGui = player.PlayerGui.PromptGui
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local accClone = ""
+local soluongGem = ""
+local gameID = 17017769292
+local data = {}
+local promptOverlay = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui") and game:GetService("CoreGui").RobloxPromptGui:FindFirstChild("promptOverlay")
+local connection
 
-getgenv().autoPlay = {
-    difficulty = "Nightmare", -- difficulty it will vote for
-    map = "TimeFactory",      -- map name u want to play, (check the name above the thing ingame)
-    autoReplay = true,        -- play again when you die
-    skipMethod = "gui",       -- guiToggle (clicks on the auto skip button), remote (fires remote every so often), gui is more stable
-    autoSkip = true,
-    showScreen = false,
-}
-
--- updated for clock event
-getgenv().autoEasterPass = {
-    enabled = true,
-    goToLobbyAfterReachedEggs = 400, -- go back to lobby once you get this amt of eggs
-    convertEggsToGems = true,
-    claimClockQuests = true,
-}
-
-getgenv().autoBoost = { -- this will use boosts when it runs out
-    ["2xCoinsBoost"] = true,
-    ["2xLuckBoost"] = false,
-    ["2xEggsBoost"] = true,
-    ["2xClocksBoost"] = true,
-    ["2xEventXPBoost"] = true,
-}
-
-getgenv().clockGift = {
-    user = ""
-}
-
-getgenv().autoMail = {
-    enabled = true,             -- for gems
-    sendTroopsEnabled = true,
-    idToSendTo = 6061476744,    -- for gems
-    idToSendTo2 = {
-        6072811840,             -- 6007728398 -- 6007722739
-    },                          -- this is for troops
-    clockCrateId = 6007726949,  -- 6007726949 -- 6007728087 -- 6007730468 -- 6007730823
-    guardianClockid = 6051148124,
-    SpiderClockId = 6051147713, -- PendulumClockId
-    sendGemsAfterReached = 50,
-    sendOfRarity = {
-        ["Legendary"] = false,
-        ["Basic"] = false,
-        ["Epic"] = false,
-        ["Mythic"] = true,
-        ["Uncommon"] = false,
-        ["Rare"] = false,
-        ["Exclusive"] = true,
-        ["Godly"] = true,
-        ["Ultimate"] = true,
+if promptOverlay then
+    connection = promptOverlay.ChildAdded:Connect(function(child)
+        if child.Name == "ErrorPrompt" and child:FindFirstChild("MessageArea") and child.MessageArea:FindFirstChild("ErrorFrame") then
+            game:GetService("TeleportService"):Teleport(gameID)
+            connection:Disconnect()
+        end
+    end)
+end
+-- Hàm để click vào vị trí xác định
+local function ClickAtPosition(x, y)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, 1)
+    wait(0.1)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, 1)
+end
+local function SendKey(key, delay)
+    VirtualInputManager:SendKeyEvent(true, key, false, game)
+    wait(delay)
+    VirtualInputManager:SendKeyEvent(false, key, false, game)
+end
+local function checkMoneyValue()
+    local mapBorders = workspace:FindFirstChild("MapBorders")
+    if mapBorders then
+        return 1
+    elseif workspace:FindFirstChild("Lobby") then
+        return 0
+    elseif workspace:FindFirstChild("TradingLobby") then
+        return 2
+    else
+        return -1
+    end
+end
+-- Hàm để lấy vị trí trung tâm của một GUI Element
+local function GetCenterPosition(guiElement)
+    local absPos = guiElement.AbsolutePosition
+    local absSize = guiElement.AbsoluteSize
+    local centerX = absPos.X + absSize.X / 2
+    local centerY = absPos.Y + absSize.Y / 2
+    return centerX, centerY
+end
+local function printSpecificValues(key, val)
+    local specificKeys = {
+        ["Trait Crystal"] = true,
+        ["Energy Crystal"] = true,
+        ["Star Rift (Red)"] = true,
+        ["Star Rift (Blue)"] = true,
+        ["Star Rift (Yellow)"] = true,
+        ["Star Rift (Green)"] = true,
+        ["Frost Bind"] = true,
+        ["Risky Dice"] = true,
+        ["Level"] = true,
+        ["Gold"] = true,
+        ["Gems"] = true
     }
-}
 
-getgenv().autoSummon = {
-    crateType = "BasicCrate",             -- "JesterCrate", "BasicCrate"
-    summonAt = 10,                        -- summon all once you can do a certain amt
-    goToLobbyAfterReachedCoins = 1000000, -- go back and summon once you get certain amt of coins
-    useLuckBoost = true,                  -- use boost before summoning
-    deleteOfRarity = {
-        ["Legendary"] = false,
-        ["Basic"] = true,
-        ["Epic"] = true,
-        ["Uncommon"] = true,
-        ["Rare"] = true,
-        ["Mythic"] = false,
-        ["Exclusive"] = false,
-        ["Godly"] = false,
-        ["Ultimate"] = false,
-    },
-}
+    if specificKeys[key] then
+        if key == "Level" then
+            data["Basic Data"] = data["Basic Data"] or {}
+            data["Basic Data"]["Level"] = val
+        elseif key == "Gold" then
+            data["Basic Data"] = data["Basic Data"] or {}
+            data["Basic Data"]["Fragments"] = val
+        elseif key == "Gems" then
+            data["Basic Data"] = data["Basic Data"] or {}
+        	data["Basic Data"] = data["Basic Data"] or {}
+            	data["Basic Data"]["Beli"] = val
+        elseif key == "Trait Crystal" or key == "Energy Crystal" or key == "Frost Bind" or key == "Risky Dice" or
+               key == "Star Rift (Red)" or key == "Star Rift (Blue)" or key == "Star Rift (Yellow)" or key == "Star Rift (Green)" then
+            data["Items Inventory"] = data["Items Inventory"] or {}
+            data["Items Inventory"][key] = val
+        end
+    end
+end
+local function writeDataToFile()
+    local jsonData = HttpService:JSONEncode(data)
+    local beliValue = checkMoneyValue()
+    local viewportFrame = player.PlayerGui.HUD.Toolbar.UnitBar.UnitHolder.UnitGridPrefab.Button.ViewportFrame
+    local worldModel = viewportFrame:WaitForChild("WorldModel")
 
-script_key='BThxaENphDCOWpCzSSSHzNMUSmVbtcAP'
+    if worldModel and worldModel:IsA("Model") and #worldModel:GetChildren() > 0 then
+        -- Lấy con đầu tiên trong WorldModel
+        local firstChild = worldModel:GetChildren()[1]
+    
+        data["Basic Data"]["Fighting Style"] = firstChild.Name
+        data["Basic Data"]["Cost"] = player.PlayerGui.HUD.Toolbar.UnitBar.UnitHolder.UnitGridPrefab.Button.TowerCostFrame.CostLabel.Text
+    else
+        print("Không tìm thấy WorldModel hoặc không có con nào trong đó.")
+    end
 
-getgenv().macroTroops = { -- will equip these for your macro
-    -- "MicrowaveMan",
-}
-getgenv().macro = '{}'
+    data["Basic Data"]["Race"] = beliValue
+    if not data["Items Inventory"] then
+        data["Items Inventory"] = {""}
+    end
+    data["Fruits Inventory"] = {""}
 
-([[
-                This script has been licensed using Luarmor
-                  Luarmor v3.8b, Lua whitelisting system
-                           https://luarmor.net/
+    local success, errorMessage = pcall(function()
+        writefile(string.format("%sData.json", player.Name), jsonData)
+    end)
 
-      _   _      _ 
-     | |_| |_ __| |
-     | __| __/ _` |
-     | |_| || (_| |
-      \__|\__\__,_|
-                   
+    if success then
+        print(string.format("The file with name %sData.json has been written", player.Name))
+    else
+        warn("got error:", errorMessage)
+    end
+end
 
-                                                     
-                 Script ID: 13fe4382d8ee9df715ffade8953a406f
-]])
+local function printTable(tbl)
+    for key, val in pairs(tbl) do
+        if type(val) == "table" then
+            if key == "Level" or key == "Currencies" or key == "Items" then
+                printTable(val)
+            else
+                printSpecificValues(key, val)
+            end
+        else
+            printSpecificValues(key, val)
+        end
+    end
+end
+local function ClickFirstItemButtonAndConfirm()
+    local scrollingFrame = player.PlayerGui.PAGES.PlayerBoothUI.ItemsGridScrollingFrame
+    local children = scrollingFrame:GetChildren()
+    local itemButton = nil
+    for _, child in ipairs(children) do
+        if child:FindFirstChild("Button") then
+            itemButton = child:FindFirstChild("Button")
+            break
+        end
+    end
+    if itemButton then
+        local X, Y = GetCenterPosition(itemButton)
+        ClickAtPosition(X - 10, Y + 30)
+        wait(0.5)
+        local ConfirmButton = player.PlayerGui.PAGES.PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder.ConfirmButton
+        if ConfirmButton then
+            local confirmX, confirmY = GetCenterPosition(ConfirmButton)
+            ClickAtPosition(confirmX - 10, confirmY + 30)
+	    wait(1)
+		local PromptGui = player.PlayerGui.PromptGui
+		local SellTextBox = PromptGui.PromptDefault.Holder.SellTextBox.TextBoxHolder.TextBox
+		SellTextBox.Text = soluongGem
+		soluongGem = ""
+		local SellButtonPath = PromptGui.PromptDefault.Holder.Options.Sell
+		wait(1)
+		local XSell, YSell = GetCenterPosition(SellButtonPath)
+      		ClickAtPosition(XSell - 10, YSell + 30)
+        end
+    end
+end
+local function MoveCharacterToFakeBooth()
+    local fxFolder = workspace.FX
+    local fxModels = fxFolder:GetChildren()
+    local fakeBoothModel = nil
+    for _, model in ipairs(fxModels) do
+        if model.Name == "FakeBooth" then
+            fakeBoothModel = model
+            break
+        end
+    end
+    local rootPart = fakeBoothModel:FindFirstChild("Root")
+    if not rootPart or not rootPart:IsA("BasePart") then
+        print("Không tìm thấy phần Root hợp lệ trong model FakeBooth")
+        return
+    end
+    
+    local targetCFrame = rootPart.CFrame
+    
+    local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+    character:SetPrimaryPartCFrame(targetCFrame)
+    wait(1)
+    SendKey(Enum.KeyCode.E, 2)
+    wait(1)
+    SendKey(Enum.KeyCode.E, 0.2)
+    wait(1)
+    local button = game:GetService("Players").LocalPlayer.PlayerGui.PAGES.PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder.Button
+    if button then
+    local X, Y = GetCenterPosition(button)
+            ClickAtPosition(X - 10, Y + 30)
+    wait(0.5)
+    ClickFirstItemButtonAndConfirm()
+else
+    print("Không tìm thấy button trong PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder")
+end
+end
+local function CheckAndClickLockButton()
+    local TradeTransactionUI = game.Players.LocalPlayer.PlayerGui.UI.TradeTransactionUI
+    local ReceiveContents = TradeTransactionUI.ReceiveContents
+    
+    if ReceiveContents.Locked.Enabled then
+        local LockButton = TradeTransactionUI.Lock
+        if LockButton then
+            local X, Y = GetCenterPosition(LockButton)
+            ClickAtPosition(X - 10, Y + 30)
+            return true
+        end
+    end
+    return false 
+end
+-- nhận tin nhắn tên clone và lưu số lượng gem ra
+local function CheckAndPrintSender()
+    local RCTScrollContentView = game:GetService("CoreGui").ExperienceChat.appLayout.chatWindow.scrollingView.bottomLockedScrollView.RCTScrollView.RCTScrollContentView
+    for i = #RCTScrollContentView:GetChildren(), 1, -1 do
+        local child = RCTScrollContentView:GetChildren()[i]
+        if child:IsA("Frame") and child.TextLabel and child.TextLabel.TextMessage then
+            local messageText = child.TextLabel.TextMessage.Text
+            local sender, message = messageText:match("%[From (.-)%].-</font>  DG(.+)")
+            if sender and message and sender ~= "" and message ~= "" then
+                accClone = sender
+                soluongGem = message
+                MoveCharacterToFakeBooth()
+                child.TextLabel.TextMessage.Text = ""
+                break
+            end
+        end
+    end
+end
+-- Hàm kiểm tra và click vào nút ResponseYes nếu có thông báo
+local function CheckAndClickResponseYes()
+    local notificationsHolder = player.PlayerGui:FindFirstChild("UI"):FindFirstChild("NotificationsHolder")
+    if notificationsHolder then
+        for _, notification in pairs(notificationsHolder:GetChildren()) do
+            if notification:IsA("Frame") and notification:FindFirstChild("PromptWindow") then
+                local promptWindow = notification.PromptWindow
+                if promptWindow:FindFirstChild("Responses") and promptWindow.Responses:FindFirstChild("ResponseYes") then
+                  local checkSender = PromptWindow.PromptMessage.Text:match("^([%a%d_]+)%s+wants")
+                    if not isYes and accClone ~= "" and checkSender ~= accClone then
+                        local responseYesButton = promptWindow.Responses.ResponseYes
+                        local centerX, centerY = GetCenterPosition(responseYesButton)
+                        ClickAtPosition(centerX - 20, centerY + 40)
+                        print("Đã click vào nút ResponseYes")
+			accClone = ""
+                        return
+                    end
+                end
+            end
+        end
+    end
+end
 
-{(function(b)if lrm_load_script then lrm_load_script('13fe4382d8ee9df715ffade8953a406f') while wait(1) do end end local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'13fe4382d8ee9df715ffade8953a406f',d}end end})end)''}(function(b)local b=b[1]local c=''local d=24915;local e=0;local f={}while e<966 do e=e+1;while e<605 and d%5260<2630 do e=e+1;d=(d-466)%41090;local b=e+d;if(d%3504)>1752 then d=(d-706)%30483;while e<313 and d%7102<3551 do e=e+1;d=(d*411)%43087;local b=e+d;if(d%14970)>7485 then d=(d*355)%41240;local b=76830;if not f[b]then f[b]=1;c=c..'.n'end elseif d%2~=0 then d=(d*316)%34377;local b=51374;if not f[b]then f[b]=1;c=c..'luarmor'end else d=(d*939)%2412;e=e+1;local b=67052;if not f[b]then f[b]=1 end end end elseif d%2~=0 then d=(d+986)%9320;while e<597 and d%3566<1783 do e=e+1;d=(d-950)%7147;local b=e+d;if(d%4020)<2010 then d=(d+826)%6247;local b=79206;if not f[b]then f[b]=1;c=c..'ht'end elseif d%2~=0 then d=(d+737)%14175;local b=51471;if not f[b]then f[b]=1;c=c..'tp'end else d=(d*705)%44808;e=e+1;local b=58695;if not f[b]then f[b]=1;c=c..'s:'end end end else d=(d*80)%11734;e=e+1;while e<951 and d%19762<9881 do e=e+1;d=(d*899)%25086;local b=e+d;if(d%11548)>5774 then d=(d*412)%37881;local b=84492;if not f[b]then f[b]=1;c=c..'//'end elseif d%2~=0 then d=(d-419)%22808;local b=94003;if not f[b]then f[b]=1;c=c..'ap'end else d=(d-851)%30011;e=e+1;local b=21386;if not f[b]then f[b]=1;c=c..'i.'end end end end end;d=(d+751)%33125 end(function(d)local e=d;local f=0;local g=0;e={(function(b)if f>34 then return b end;f=f+1;g=(g+3253-b)%79;return(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(26)c=c..'il'end;return true end)'igRzv'and e[2](687+b))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(103)c=c..'.l'end;return true end)'TilxU'and e[3](b+101))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(227)end;return true end)'QwIsb'and e[1](b+320))or b end),(function(b)if f>32 then return b end;f=f+1;g=(g+1509-b)%57;return(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(139)c=c..'et'end;return true end)'aOitw'and e[3](909+b))or(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(65)end;return true end)'ObBbT'and e[1](b+129))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(153)c=c..'ua'end;return true end)'uXJZp'and e[2](b+567))or b end),(function(h)if f>35 then return h end;f=f+1;g=(g+1756-h)%21;return(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(2)c=c..'/f'end;return true end)'bdEJm'and e[1](782+h))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(108)c=c..'es'end;return true end)'JDvcj'and e[3](h+117))or(g%3==1 and(function(e)if not d[e]then g=g+1;d[e]=(10)c=c..'/v3/l/'..b end;return true end)'tIFsS'and e[2](h+748))or h end)}e[2](8832)end){}loadstring(game:HttpGet(c)){}end)
+local function ClickButtonBack()
+    local promptScreenGui = player.PlayerGui:FindFirstChild("PromptGui")
+    if promptScreenGui then
+        local promptDefault = promptScreenGui:FindFirstChild("PromptDefault")
+        if promptDefault then
+            local backButton = promptDefault.Holder.Options:FindFirstChild("Back")
+            if backButton and backButton.Name == "Back" then
+                local X, Y = GetCenterPosition(backButton)
+                    ClickAtPosition(X - 20, Y + 40)
+                    print("Đã click vào nút Back")
+            end
+        end
+    end
+end
+repeat
+    wait(2)
+    local moneyValue = checkMoneyValue()
+until moneyValue ~= -1
+spawn(function()
+while true do
+        local success, value = pcall(function() return getInventoryRemote:InvokeServer() end)
+        if success then
+            printTable(value)
+            writeDataToFile()
+        else
+            warn("Không thể nhận giá trị từ server: " .. tostring(value))
+        end
+        wait(10)
+    end
+end)
+local moneyValue = checkMoneyValue()
+
+if moneyValue == 0 then
+    game:GetService("TeleportService"):Teleport(17490500437)
+else
+    game:GetService("CoreGui").ExperienceChat.appLayout.Visible = false
+    game:GetService("CoreGui").PlayerList.PlayerListMaster.Visible = false
+    spawn(function()
+  while true do
+      CheckAndPrintSender()
+      wait(3)
+  end
+end)
+spawn(function()
+  while true do
+    CheckAndClickResponseYes()
+    wait(1)
+  end
+end)
+spawn(function()
+  while true do
+    ClickButtonBack()
+    wait(1)
+  end
+end)
+end
+spawn(function()
+    while true do
+        CheckAndClickLockButton()
+        wait(2) 
+    end
+end)
