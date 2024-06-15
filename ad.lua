c889496dac43eee4eb9a88b52815718234f721ed
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-06-15 22:31:05 +0700

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -1,316 +1,83 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local getInventoryRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("GetInventory")
-local HttpService = game:GetService("HttpService")
-local CoreGui = game:GetService("CoreGui")
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local Mouse = player:GetMouse()
-local promptGui = player.PlayerGui.PromptGui
-local VirtualInputManager = game:GetService("VirtualInputManager")
-local accClone = ""
-local soluongGem = ""
-local gameID = 17017769292
-local data = {}
-local promptOverlay = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui") and game:GetService("CoreGui").RobloxPromptGui:FindFirstChild("promptOverlay")
-local connection
+repeat
+    wait()
+until game:IsLoaded()
+function ClickMiddle()
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(workspace.CurrentCamera.ViewportSize.X/2,workspace.CurrentCamera.ViewportSize.Y/2,0,true,a,1)
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(workspace.CurrentCamera.ViewportSize.X/2,workspace.CurrentCamera.ViewportSize.Y/2,0,false,a,1)    
+end 
+repeat
+    wait()
+until game.Players.LocalPlayer
 
-if promptOverlay then
-    connection = promptOverlay.ChildAdded:Connect(function(child)
-        if child.Name == "ErrorPrompt" and child:FindFirstChild("MessageArea") and child.MessageArea:FindFirstChild("ErrorFrame") then
-            game:GetService("TeleportService"):Teleport(gameID)
-            connection:Disconnect()
-        end
-    end)
-end
--- Hàm để click vào vị trí xác định
-local function ClickAtPosition(x, y)
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, 1)
-    wait(0.1)
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, 1)
-end
-local function SendKey(key, delay)
-    VirtualInputManager:SendKeyEvent(true, key, false, game)
-    wait(delay)
-    VirtualInputManager:SendKeyEvent(false, key, false, game)
-end
-local function checkMoneyValue()
-    local mapBorders = workspace:FindFirstChild("MapBorders")
-    if mapBorders then
-        return 1
-    elseif workspace:FindFirstChild("Lobby") then
-        return 0
-    elseif workspace:FindFirstChild("TradingLobby") then
-        return 2
-    else
-        return -1
-    end
-end
--- Hàm để lấy vị trí trung tâm của một GUI Element
-local function GetCenterPosition(guiElement)
-    local absPos = guiElement.AbsolutePosition
-    local absSize = guiElement.AbsoluteSize
-    local centerX = absPos.X + absSize.X / 2
-    local centerY = absPos.Y + absSize.Y / 2
-    return centerX, centerY
-end
-local function printSpecificValues(key, val)
-    local specificKeys = {
-        ["Trait Crystal"] = true,
-        ["Energy Crystal"] = true,
-        ["Star Rift (Red)"] = true,
-        ["Star Rift (Blue)"] = true,
-        ["Star Rift (Yellow)"] = true,
-        ["Star Rift (Green)"] = true,
-        ["Frost Bind"] = true,
-        ["Risky Dice"] = true,
-        ["Level"] = true,
-        ["Gold"] = true,
-        ["Gems"] = true
-    }
+local plr = game.Players.LocalPlayer
 
-    if specificKeys[key] then
-        if key == "Level" then
-            data["Basic Data"] = data["Basic Data"] or {}
-            data["Basic Data"]["Level"] = val
-        elseif key == "Gold" then
-            data["Basic Data"] = data["Basic Data"] or {}
-            data["Basic Data"]["Fragments"] = val
-        elseif key == "Gems" then
-            data["Basic Data"] = data["Basic Data"] or {}
-        	data["Basic Data"] = data["Basic Data"] or {}
-            	data["Basic Data"]["Beli"] = val
-        elseif key == "Trait Crystal" or key == "Energy Crystal" or key == "Frost Bind" or key == "Risky Dice" or
-               key == "Star Rift (Red)" or key == "Star Rift (Blue)" or key == "Star Rift (Yellow)" or key == "Star Rift (Green)" then
-            data["Items Inventory"] = data["Items Inventory"] or {}
-            data["Items Inventory"][key] = val
-        end
-    end
-end
-local function writeDataToFile()
-    local jsonData = HttpService:JSONEncode(data)
-    local beliValue = checkMoneyValue()
-    local viewportFrame = player.PlayerGui.HUD.Toolbar.UnitBar.UnitHolder.UnitGridPrefab.Button.ViewportFrame
-    local worldModel = viewportFrame:WaitForChild("WorldModel")
+repeat
+    wait()
+until plr.Character
 
-    if worldModel and worldModel:IsA("Model") and #worldModel:GetChildren() > 0 then
-        -- Lấy con đầu tiên trong WorldModel
-        local firstChild = worldModel:GetChildren()[1]
-    
-        data["Basic Data"]["Fighting Style"] = firstChild.Name
-        data["Basic Data"]["Cost"] = player.PlayerGui.HUD.Toolbar.UnitBar.UnitHolder.UnitGridPrefab.Button.TowerCostFrame.CostLabel.Text
-    else
-        print("Không tìm thấy WorldModel hoặc không có con nào trong đó.")
-    end
+repeat
+    wait()
+until plr.Character:FindFirstChild("HumanoidRootPart")
 
-    data["Basic Data"]["Race"] = beliValue
-    if not data["Items Inventory"] then
-        data["Items Inventory"] = {""}
-    end
-    data["Fruits Inventory"] = {""}
-
-    local success, errorMessage = pcall(function()
-        writefile(string.format("%sData.json", player.Name), jsonData)
-    end)
-
-    if success then
-        print(string.format("The file with name %sData.json has been written", player.Name))
-    else
-        warn("got error:", errorMessage)
-    end
+repeat
+    wait()
+until plr.Character:FindFirstChild("Humanoid")
+repeat wait()
+    ClickMiddle()
+until game:GetService("Players").LocalPlayer.PlayerGui.Loading.Enabled == true
+_G.farm = true
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+wait(1)
+vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+function ClickButton(a)
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,true,a,1)
+    game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,false,a,1)
 end
 
-local function printTable(tbl)
-    for key, val in pairs(tbl) do
-        if type(val) == "table" then
-            if key == "Level" or key == "Currencies" or key == "Items" then
-                printTable(val)
-            else
-                printSpecificValues(key, val)
-            end
-        else
-            printSpecificValues(key, val)
-        end
-    end
-end
-local function ClickFirstItemButtonAndConfirm()
-    local scrollingFrame = player.PlayerGui.PAGES.PlayerBoothUI.ItemsGridScrollingFrame
-    local children = scrollingFrame:GetChildren()
-    local itemButton = nil
-    for _, child in ipairs(children) do
-        if child:FindFirstChild("Button") then
-            itemButton = child:FindFirstChild("Button")
-            break
-        end
-    end
-    if itemButton then
-        local X, Y = GetCenterPosition(itemButton)
-        ClickAtPosition(X - 10, Y + 30)
-        wait(0.5)
-        local ConfirmButton = player.PlayerGui.PAGES.PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder.ConfirmButton
-        if ConfirmButton then
-            local confirmX, confirmY = GetCenterPosition(ConfirmButton)
-            ClickAtPosition(confirmX - 10, confirmY + 30)
-	    wait(1)
-		local PromptGui = player.PlayerGui.PromptGui
-		local SellTextBox = PromptGui.PromptDefault.Holder.SellTextBox.TextBoxHolder.TextBox
-		SellTextBox.Text = soluongGem
-		soluongGem = ""
-		local SellButtonPath = PromptGui.PromptDefault.Holder.Options.Sell
-		wait(1)
-		local XSell, YSell = GetCenterPosition(SellButtonPath)
-      		ClickAtPosition(XSell - 10, YSell + 30)
-        end
-    end
-end
-local function MoveCharacterToFakeBooth()
-    local fxFolder = workspace.FX
-    local fxModels = fxFolder:GetChildren()
-    local fakeBoothModel = nil
-    for _, model in ipairs(fxModels) do
-        if model.Name == "FakeBooth" then
-            fakeBoothModel = model
-            break
-        end
-    end
-    local rootPart = fakeBoothModel:FindFirstChild("Root")
-    if not rootPart or not rootPart:IsA("BasePart") then
-        print("Không tìm thấy phần Root hợp lệ trong model FakeBooth")
-        return
-    end
-    
-    local targetCFrame = rootPart.CFrame
-    
-    local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
-    character:SetPrimaryPartCFrame(targetCFrame)
-    wait(1)
-    SendKey(Enum.KeyCode.E, 2)
-    wait(1)
-    SendKey(Enum.KeyCode.E, 0.2)
-    wait(1)
-    local button = game:GetService("Players").LocalPlayer.PlayerGui.PAGES.PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder.Button
-    if button then
-    local X, Y = GetCenterPosition(button)
-            ClickAtPosition(X - 10, Y + 30)
-    wait(0.5)
-    ClickFirstItemButtonAndConfirm()
-else
-    print("Không tìm thấy button trong PlayerBoothUI.BottomHolder.ModeHolder.ResponseHolder")
-end
-end
-local function CheckAndClickLockButton()
-    local TradeTransactionUI = game.Players.LocalPlayer.PlayerGui.UI.TradeTransactionUI
-    local ReceiveContents = TradeTransactionUI.ReceiveContents
-    
-    if ReceiveContents.Locked.Enabled then
-        local LockButton = TradeTransactionUI.Lock
-        if LockButton then
-            local X, Y = GetCenterPosition(LockButton)
-            ClickAtPosition(X - 10, Y + 30)
-            return true
-        end
-    end
-    return false 
-end
--- nhận tin nhắn tên clone và lưu số lượng gem ra
-local function CheckAndPrintSender()
-    local RCTScrollContentView = game:GetService("CoreGui").ExperienceChat.appLayout.chatWindow.scrollingView.bottomLockedScrollView.RCTScrollView.RCTScrollContentView
-    for i = #RCTScrollContentView:GetChildren(), 1, -1 do
-        local child = RCTScrollContentView:GetChildren()[i]
-        if child:IsA("Frame") and child.TextLabel and child.TextLabel.TextMessage then
-            local messageText = child.TextLabel.TextMessage.Text
-            local sender, message = messageText:match("%[From (.-)%].-</font>  DG(.+)")
-            if sender and message and sender ~= "" and message ~= "" then
-                accClone = sender
-                soluongGem = message
-                MoveCharacterToFakeBooth()
-                child.TextLabel.TextMessage.Text = ""
-                break
-            end
-        end
-    end
-end
--- Hàm kiểm tra và click vào nút ResponseYes nếu có thông báo
-local function CheckAndClickResponseYes()
-    local notificationsHolder = player.PlayerGui:FindFirstChild("UI"):FindFirstChild("NotificationsHolder")
-    if notificationsHolder then
-        for _, notification in pairs(notificationsHolder:GetChildren()) do
-            if notification:IsA("Frame") and notification:FindFirstChild("PromptWindow") then
-                local promptWindow = notification.PromptWindow
-                if promptWindow:FindFirstChild("Responses") and promptWindow.Responses:FindFirstChild("ResponseYes") then
-                  local checkSender = PromptWindow.PromptMessage.Text:match("^([%a%d_]+)%s+wants")
-                    if not isYes and accClone ~= "" and checkSender ~= accClone then
-                        local responseYesButton = promptWindow.Responses.ResponseYes
-                        local centerX, centerY = GetCenterPosition(responseYesButton)
-                        ClickAtPosition(centerX - 20, centerY + 40)
-                        print("Đã click vào nút ResponseYes")
-			accClone = ""
-                        return
-                    end
+
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    if _G.farm  then
+        if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+            for i,v in next,game.Players.LocalPlayer.Character:GetDescendants() do 
+                if (v:IsA("Part") or v:IsA("MeshPart")) and  v.CanCollide then 
+                    v.CanCollide = false 
                 end
             end
         end
     end
-end
-
-local function ClickButtonBack()
-    local promptScreenGui = player.PlayerGui:FindFirstChild("PromptGui")
-    if promptScreenGui then
-        local promptDefault = promptScreenGui:FindFirstChild("PromptDefault")
-        if promptDefault then
-            local backButton = promptDefault.Holder.Options:FindFirstChild("Back")
-            if backButton and backButton.Name == "Back" then
-                local X, Y = GetCenterPosition(backButton)
-                    ClickAtPosition(X - 20, Y + 40)
-                    print("Đã click vào nút Back")
-            end
+end)    
+print(1)
+while _G.farm do wait()
+pcall(function()
+    if string.find(tostring(game.Players.LocalPlayer.Team),"Giao") then
+        if game.Players.LocalPlayer.Character:FindFirstChild("Cardboard_Box") then 
+            
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.DeliveryBeam.Attachment1.WorldCFrame
+            wait(.5)
+            fireproximityprompt(game.Players.LocalPlayer.Character.DeliveryBeam.Attachment1.ProximityPrompt)
+            wait(15)
+        else 
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  workspace.Map.Delivery.Cardboard_Box.Box.CFrame
+            wait(.5)
+            fireproximityprompt(workspace.Map.Delivery.Cardboard_Box.Box.ProximityPrompt)
+            wait(.5)
         end
+    else
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Map.Delivery["Giao H\195\160ng"].HumanoidRootPart.CFrame*CFrame.new(0,0,-1)
+        wait(.25)
+        if game:GetService("Players").LocalPlayer.PlayerGui.Interact.Option.Visible then
+            ClickButton(game:GetService("Players").LocalPlayer.PlayerGui.Interact.Option.Yes.Button)
+            wait(.5)
+        else 
+            ClickMiddle()
+            wait(.5)
+        end 
+        
+            
     end
-end
-repeat
-    wait(2)
-    local moneyValue = checkMoneyValue()
-until moneyValue ~= -1
-spawn(function()
-while true do
-        local success, value = pcall(function() return getInventoryRemote:InvokeServer() end)
-        if success then
-            printTable(value)
-            writeDataToFile()
-        else
-            warn("Không thể nhận giá trị từ server: " .. tostring(value))
-        end
-        wait(10)
-    end
-end)
-local moneyValue = checkMoneyValue()
-
-if moneyValue == 0 then
-    game:GetService("TeleportService"):Teleport(17490500437)
-else
-    game:GetService("CoreGui").ExperienceChat.appLayout.Visible = false
-    game:GetService("CoreGui").PlayerList.PlayerListMaster.Visible = false
-    spawn(function()
-  while true do
-      CheckAndPrintSender()
-      wait(3)
-  end
-end)
-spawn(function()
-  while true do
-    CheckAndClickResponseYes()
-    wait(1)
-  end
-end)
-spawn(function()
-  while true do
-    ClickButtonBack()
-    wait(1)
-  end
 end)
 end
-spawn(function()
-    while true do
-        CheckAndClickLockButton()
-        wait(2) 
-    end
-end)
