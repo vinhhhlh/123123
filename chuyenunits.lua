9fbcbf0260f401b4a8e11e2f595122a9157bc87b
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-06-29 11:12:08 +0700

    Update chuyenunits.lua

--- chuyenunits.lua
+++ chuyenunits.lua
@@ -1,45 +1,33 @@
 local users = {
-    "OFKvxbpuGhu",
-    "rdHzGwPQLSX",
-    "DNshmAcIGxq",
-    "NfBdJCGGzKJ",
-    "TwvcTuPSUZW",
-    "zAIELefLLcb",
-    "tbMqYwCcmwl",
-    "sfPnklUUwze",
-    "GQbYlGqPCwl",
-    "WcBilnlIRRd",
-    "QuxUbKUKSlt",
-    "vPVzRppRnDY",
-    "uTfFQaDbrCZ",
-    "KQIiwFwUAGU",
-    "NjinGvANSEW",
-    "ZLOGfcIIusa",
-    "JsyWgmBlzlj",
-    "bNcnYmLyXvS",
-    "GBcttfuyJKs",
-    "rgcWKOHSDMJ",
-    "jYcbzleGkJT",
-    "OhRUoTbWWKu",
-    "GYXgNGnNENO",
-    "OEQTQkdakZN",
-    "oCmVdrtQfha",
-    "NPdvLyWnCXL",
-    "HFQDbMAXayM",
-    "edaeolnzMgf",
-    "WfngKKuYZIE",
-    "MpTAniasxOX",
-    "yiEhDCfclFr",
-    "TtSmuTkpDke",
-    "PrSNGavlLIJ",
-    "ONOAmIJcasX",
-    "nbxPgsybJZs",
-    "PeWjWGlIZSI",
-    "QHwEkatxOCe",
-    "jhudUCJGEdf",
-    "scOwNmoiNTK",
-    "uSrxpbJwmuK"
-    
+    "hJKgqvDPF",
+    "jiswHzgoVCs",
+    "oDTMTWzGluJ",
+    "aNEdttwuvgJ",
+    "uXSNbLdXvID",
+    "jymiQIesOFJ",
+    "fgDRvDHxuJX",
+    "XHPkfsJquoU",
+    "nkCwUaMZAHB",
+    "AlUsYyWqqil",
+    "tIzUHRMFLLa",
+    "vgByDyLmVCm",
+    "mOFcizoEBfX",
+    "RjbsgRIRYAm",
+    "mgkfFHaPTFb",
+    "GQgaAPWKvsM",
+    "VaHsotIUgcp",
+    "yBPPQfJlbfw",
+    "cuIgdpcYaRz",
+    "AvKqMKSKZoQ",
+    "WFCOSkRQhDE",
+    "MqJLrTESVNg",
+    "PLVvbUMnOxB",
+    "XlXOoidsCje",
+    "IMArNGzTkgg",
+    "dIEHARNEBvm",
+    "WpLydesmghm",
+    "uYXJrFMyZhM",
+    "SWMoxVjZmTc",
 }
 
 local troopsToSend = {
@@ -49,16 +37,14 @@ local troopsToSend = {
     "GuardianClockman",
 }
 
-
 local TTD
-local save
-local handler
 local Network
 local Invoke
 local Fire
+
 task.spawn(function()
     TTD = require(game:GetService("ReplicatedStorage").MultiboxFramework)
-    save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
+    local save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
 
     repeat
         pcall(function()
@@ -70,27 +56,8 @@ task.spawn(function()
     until Network ~= nil and Invoke ~= nil and Fire ~= nil
 end)
 
-Invoke = Network.Invoke; local GetFunc = getupvalue(Invoke, 1)
-Fire = Network.Fire; local GetEvent = getupvalue(Fire, 1)
-
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetFunc, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
-
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetEvent, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
-
-
 local invTroops = {}
+
 function getInventoryTroops()
     invTroops = {}
     local save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
@@ -103,6 +70,7 @@ function getInventoryTroops()
 end
 
 local coins
+
 function getCoinAmt()
     coins = 0
     local save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
@@ -115,8 +83,8 @@ function getCoinAmt()
 end
 
 function hasTroop(id)
-    troops = getInventoryTroops()
-    for i, v in troops do
+    local troops = getInventoryTroops()
+    for i, v in pairs(troops) do
         if i == id then
             return true
         end
@@ -124,26 +92,23 @@ function hasTroop(id)
     return false
 end
 
-startAmt = getCoinAmt()
+local startAmt = getCoinAmt()
 
-local amt = 0
-
-for i, user in users do
+for _, user in ipairs(users) do
     local sent = {}
-    for i, v in getInventoryTroops() do
+    for i, v in pairs(getInventoryTroops()) do
         if table.find(troopsToSend, v) and not table.find(sent, v) then
             table.insert(sent, v)
             local oldC = getCoinAmt()
             local st = tick()
             repeat
-                Invoke("PostOffice_SendGift", game.Players:GetUserIdFromNameAsync(user), "Troops", i, 0,
-                    tostring(math.random(1, 10000)))
+                Invoke("PostOffice_SendGift", game.Players:GetUserIdFromNameAsync(user), "Troops", i, 0, tostring(math.random(1, 10000)))
                 task.wait(0.1)
             until getCoinAmt() < oldC and not hasTroop(i)
-            print("sent","time taken:",tick()-st)
+            print("sent", "time taken:", tick() - st)
         end
     end
-    print('finished user:',user)
+    print('finished user:', user)
 end
 
 print('Should have sent:', amt)
