5e077a35693a130d3510c09724a1a49d5c34f301
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-08-11 19:30:31 +0700

    Update autobuy.lua

--- autobuy.lua
+++ autobuy.lua
@@ -1,56 +1,157 @@
-getgenv().Key = "k3d5cc04abcc9f3116173d57"
-getgenv().BountyFarm = {
-    ["Script"] = {
-        ["Black Screen"] = true,
-        ["Team"] = "Pirates", -- Pirates/Marines
-        ["Panic % Health"] = 20, -- Teleport to Safe Zone if player below x % HP
-        ["Skip Timer"] = 60, -- Skip if took more than x seconds to kill
-        ["M1 Health"] = 90, -- Attack M1 if target health above x % HP
-        ["Ignore User"] = {"Portal-Portal", "Kitsune-Kitsune", "Mammoth-Mammoth"}, -- Ignore user with the ate Blox Fruit
-        ["Ignore Bounty"] = 5000000, -- Ignore user with higher than x bounty than you
-    },
-    ["Hop Server"] = {
-        ["Delay"] = 10, -- Delay before Hop Server
-        ["Below Ping"] = 100, -- Hop to Server below x ping
-        ["Hop Ping"] = 1000, -- Hop if current server ping is higher than x 
-    },
-    ["Skill"] = {
-        ["Cycle"] = true, -- Use in a Cycle (making use used all skill) instead of use prioritize the first order
-        ["Prioritize"] = {"Melee", "Blox Fruit", "Sword", "Gun"}, -- Use skill Left to right (first to last)
-        -- ["Skill"] = {Enable, Predict, Hold, Delay Next Skill}
-        ["Melee"] = {
-            ["Z"] = {true, true, 0.2, 0.1},
-            ["X"] = {true, true, 0.2, 0.1},
-            ["C"] = {true, true, 0.5, 0.1},
-        },
-        ["Blox Fruit"] = {
-            ["Z"] = {false, true, 0, 1},
-            ["X"] = {false, true, 0, 1},
-            ["C"] = {false, true, 0, 1},
-            ["V"] = {false, true, 0, 1},
-            ["F"] = {false, true, 0, 1},
-        },
-        ["Sword"] = {
-            ["Z"] = {true, true, 0.8, 0},
-            ["X"] = {true, true, 0.2, 0},
-        },
-        ["Gun"] = {
-            ["Z"] = {false, true, 0, 1},
-            ["X"] = {false, true, 0, 1},
-        },
-    },
+script_key = "BThxaENphDCOWpCzSSSHzNMUSmVbtcAP"
+getgenv().Team = "Pirates"
+getgenv().WebhookSetting = {
+    Enable = false,
+    Url = "",
+    Embed = true,
+    StoredFruit = true,
+    ImageEmbed = true,
+    CustomImage = false,
+    CustomImageUrl = "", --Your Url
+    OnServerHop = true,
+    BountyChanged = true,
 }
-getgenv().Setup = {
-    ["Melee"]  = "Dragon Talon",
-    ["Sword"] = "Cursed Dual Katana",
-    ["Gun"] = "Soul Guitar",
-    ["Accessory"] = "Warrior Helmet",
-    ["Stat"] = {
-        ["Melee"] = 2550,
-        ["Defense"] = 2550,
-        ["Sword"] = 2550,
-        ["Gun"] = 0,
-        ["Blox Fruit"] = 0,
+getgenv().PlayerSetting = {
+    SafeMode = true,
+    SafeModeHealth = {3000,75},--Number And %, Start Safe Mode And Stop Safe Mode
+    UseRaceV3 = true,
+    SmartUseRaceV3 = true,
+    DashIfV4 = true,
+    Dash = true,
+    IgnoreInCombat = true, --Turn This Off When Reseting Or Hop You Lost Bounty (Rare, Happens On Some Accounts)
+    ChatKillEnable = true,
+    Chat = {"Cf by MrBear","MrBear is one"},
+    IgnoreFriends = true, --Serverhop if you friend in your server
+}
+getgenv().AttackSetting = {
+    ForceMelee = true,
+    ForceMeleeTime = 3,
+    StopAttack = true, --When Meet Below Condition
+    StopAttackAtHealth = 80,--%
+    FastAttack = true, -- Toggle Fast Attack
+}
+getgenv().UseSkillSetting = {
+    -- Three Methods: "Normal", "Fast", "Spam", "SpamAll"
+    MethodIfTargetOnV4 = "Fast",
+    MethodIfPlayerOnV4 = "SpamAll",
+    MethodIfTargetUseFruit = {Fruits={Buddha,Portal},Method="Fast"},
+    NormalMethod = "Normal",
+    LowHealthPlayerCondition = { --Player Can Attack Us, No Need For Slow Attack
+        Enable = true,
+        Health = 70,--%Health That Are Low
+        Method = "Fast",
+    },
+    LowHealthTargetCondition = {
+        Enable = true,
+        Health = 20, --%Health That Are Low
+        DelayFirstTime = {true,2}, --1 Is Enable, 2 Is Second To Delay Before Attack Again
+        Method = "Normal",
+        WaitTime = 1.1, -- If Normal Method, Wait Every Skill If It Hits Target
     }
 }
-repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until Joebiden
+getgenv().WeaponsSetting = {
+    ["Melee"] = {
+        ["Enable"] = true,
+        ["Delay"] = 1, 
+        ["SwitchNextWeaponIfCooldown"] = true,
+        ["Skills"] = {
+            ["Z"] = {
+                ["Enable"] = true,
+                ["NoPredict"] = false, -- For Dragon Tailon, Disable it 
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0.1,
+            },
+        [ "X"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0.1,
+            },
+
+            ["C"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.5,
+                ["TimeToNextSkill"] = 0.1,
+            },
+        },
+    },
+    ["Blox Fruit"] = {
+        ["Enable"] = false,
+        ["Delay"] = 2,
+        ["SwitchNextWeaponIfCooldown"] = false,
+        ["Skills"] = {
+            ["Z"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0.1,
+            },
+            ["X"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0.1,
+            },
+
+            ["C"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0.1,
+            },
+            ["V"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.1,
+                ["TimeToNextSkill"] = 0.1,
+            },
+            ["F"] = {
+                ["Enable"] = false,
+                ["HoldTime"] = 0,
+                ["TimeToNextSkill"] = 0,
+            },
+        },
+    },
+    ["Sword"] = {
+        ["Enable"] = true,
+        ["Delay"] = 1.2,
+        ["Skills"] = {
+            ["Z"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.8,
+                ["TimeToNextSkill"] = 0,
+            },
+            ["X"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.2,
+                ["TimeToNextSkill"] = 0,
+            },
+        },
+    },
+    ["Gun"] = {
+        ["Enable"] = false,
+        ["Delay"] = 0.5,
+        ["Skills"] = {
+            ["Z"] = {
+                ["Enable"] = false,
+                ["HoldTime"] = 0.1,
+                ["TimeToNextSkill"] = 0,
+            },
+            ["X"] = {
+                ["Enable"] = true,
+                ["HoldTime"] = 0.1,
+                ["TimeToNextSkill"] = 0,
+            },
+        },
+    },
+}
+getgenv().Theme = { -- getgenv().Theme = false if you want to disable
+    OldTheme = true,
+    Name="Miko", --"Raiden","Ayaka","Hutao","Yelan","Miko","Nahida","Ganyu","Keqing","Nilou","Barbara","Zhongli","Layla"
+    Custom={
+            ["Enable"] = false,
+            ['char_size'] = UDim2.new(0.668, 0, 1.158, 0),
+            ['char_pos'] = UDim2.new(0.463, 0, -0.105, 0),
+            ['title_color'] = Color3.fromRGB(255, 221, 252),
+            ['titleback_color'] = Color3.fromRGB(169, 20, 255),
+            ['list_color'] = Color3.fromRGB(255, 221, 252),
+            ['liststroke_color'] = Color3.fromRGB(151, 123, 207),
+            ['button_color'] = Color3.fromRGB(255, 221, 252)
+       }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/248f97d7a28a4d09c641d8279a935333.lua"))()
