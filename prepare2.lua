86468c0e6c0dc640990a4932082a8a0ee7e4c65f
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-10-03 13:22:18 +0700

    Update prepare2.lua

--- prepare2.lua
+++ prepare2.lua
@@ -1,17 +1,10 @@
 if not getupvalue then getupvalue = debug.getupvalue end
-local users = {
-"hvQgNgrd",
 
-}
-
+local users = {"hvQgNgrd"}
 local troopsToSend = {
-    "SantaTVMan",
-    "LuckySpeakerman",
-    "ClockSpider",
-    "GuardianClockman",
-    "ScientistClockman",
+    "SantaTVMan", "LuckySpeakerman", "ClockSpider", 
+    "GuardianClockman", "ScientistClockman"
 }
-
 
 local TTD
 local save
@@ -19,40 +12,50 @@ local handler
 local Network
 local Invoke
 local Fire
+
 task.spawn(function()
     TTD = require(game:GetService("ReplicatedStorage").MultiboxFramework)
     save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
 
     repeat
-        pcall(function()
+        local success, err = pcall(function()
             Network = TTD.Network
             Invoke = Network.Invoke
             Fire = Network.Fire
         end)
+        if not success then
+            warn("Error in getting Network functions: ", err)
+        end
         task.wait(0.1)
     until Network ~= nil and Invoke ~= nil and Fire ~= nil
 end)
 
-Invoke = Network.Invoke; local GetFunc = getupvalue(Invoke, 1)
-Fire = Network.Fire; local GetEvent = getupvalue(Fire, 1)
+-- Di chuyển getupvalue sau khi đã đảm bảo rằng Invoke và Fire không phải nil
+task.spawn(function()
+    repeat task.wait(0.1) until Invoke and Fire -- Đợi đến khi chúng có giá trị
+    Invoke = Network.Invoke
+    Fire = Network.Fire
+    local GetFunc = getupvalue(Invoke, 1)
+    local GetEvent = getupvalue(Fire, 1)
 
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetFunc, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
+    coroutine.wrap(function()
+        setidentity(2)
+        hookfunc(getupvalue(GetFunc, 1), function()
+            return true
+        end)
+        setidentity(8)
+    end)()
 
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetEvent, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
+    coroutine.wrap(function()
+        setidentity(2)
+        hookfunc(getupvalue(GetEvent, 1), function()
+            return true
+        end)
+        setidentity(8)
+    end)()
+end)
 
-
+-- Lấy danh sách quân trong kho
 local invTroops = {}
 function getInventoryTroops()
     invTroops = {}
@@ -65,6 +68,7 @@ function getInventoryTroops()
     return invTroops
 end
 
+-- Lấy số lượng coins
 local coins
 function getCoinAmt()
     coins = 0
@@ -77,9 +81,10 @@ function getCoinAmt()
     return coins
 end
 
+-- Kiểm tra xem người chơi có quân cụ thể không
 function hasTroop(id)
-    troops = getInventoryTroops()
-    for i, v in troops do
+    local troops = getInventoryTroops()
+    for i, v in pairs(troops) do
         if i == id then
             return true
         end
@@ -87,26 +92,25 @@ function hasTroop(id)
     return false
 end
 
-startAmt = getCoinAmt()
-
+-- Bắt đầu xử lý gửi quà
+local startAmt = getCoinAmt()
 local amt = 0
 
-for i, user in users do
+for i, user in pairs(users) do
     local sent = {}
-    for i, v in getInventoryTroops() do
+    for i, v in pairs(getInventoryTroops()) do
         if table.find(troopsToSend, v) and not table.find(sent, v) then
             table.insert(sent, v)
             local oldC = getCoinAmt()
             local st = tick()
             repeat
-                Invoke("PostOffice_SendGift", game.Players:GetUserIdFromNameAsync(user), "Troops", i, 0,
-                    tostring(math.random(1, 10000)))
+                Invoke("PostOffice_SendGift", game.Players:GetUserIdFromNameAsync(user), "Troops", i, 0, tostring(math.random(1, 10000)))
                 task.wait(0.1)
             until getCoinAmt() < oldC and not hasTroop(i)
-            print("sent","time taken:",tick()-st)
+            print("sent", "time taken:", tick() - st)
         end
     end
-    print('finished user:',user)
+    print('finished user:', user)
 end
 
 print('Should have sent:', amt)
