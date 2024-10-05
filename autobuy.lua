7272f831a7cee098e80e8d800053acc5459b5f2a
hahaha <166713455+vinhhhlh@users.noreply.github.com>
2024-10-05 17:25:27 +0700

    Update autobuy.lua

--- autobuy.lua
+++ autobuy.lua
@@ -1,331 +1,186 @@
-local toBuy = {
-["Santa TV Man"] = { 800, 55 }, -- max price to buy for, max amt of troop in inventory
-['Lucky Speakerman'] = { 50, 55 },
-}
+-- Copyright © 2017 Quaker NTj <quakerntj@hotmail.com>
+-- <https://github.com/quakerntj/ffbe_autoscript>
 
-local items = 0
-local e
-repeat
-    task.wait(1)
-    pcall(function() e = require(game:GetService("ReplicatedStorage").MultiboxFramework) end)
-until game:IsLoaded() and #game:GetService("ReplicatedStorage"):GetDescendants() >= 26500 and e ~= nil and e.Network and e.Replicate
-game:GetService("ReplicatedStorage"):WaitForChild("MultiboxFramework")
+--[[
+	This script is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
+	This script is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-local TTD = require(game:GetService("ReplicatedStorage").MultiboxFramework)
-local save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
-local handler
-local Network = TTD.Network
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 
-local Invoke = Network.Invoke; local GetFunc = getupvalue(Invoke, 1)
-local Fire = Network.Fire; local GetEvent = getupvalue(Fire, 1)
-if TTD.debug then print('Found Invoke/Fire') end
--- $ -- Bypass
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetFunc, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
+Explorer = {}
+Explorer.__index = Explorer
 
-coroutine.wrap(function()
-    setidentity(2)
-    hookfunc(getupvalue(GetEvent, 1), function()
-        return true
-    end)
-    setidentity(8)
-end)()
+setmetatable(Explorer, {
+	__call = function (cls, ...)
+		return cls.new(...)
+	end,
+})
 
-function convertStringToNumber(str)
-    local suffixes = {["k"] = 10^3, ["m"] = 10^6}
-    local num, suffix = str:match("(%d+)([km]?)")
-    num = tonumber(num)
-    if suffixes[suffix] then
-        num = num * suffixes[suffix]
-    end
-    return num
+function Explorer.new()
+	local self = setmetatable({}, Explorer)
+	self:init()
+	return self
 end
 
-local invTroops = {}
-function getInventoryTroops()
-    invTroops = {}
-    local save = TTD.Replicate:WaitForReplica("PlayerData-" .. game:GetService("Players").LocalPlayer.UserId)
-    for name, v in pairs(save._data.Inventory.Troops) do
-        for i, v in pairs(v) do
-            invTroops[i] = name
-        end
-    end
-    return invTroops
+function Explorer:init()
+	local DBScriptList = { "explore1.dbs", "explore2.dbs", "explore3.dbs", "explore4.dbs", "explore5.dbs" }
+	dialogInit()
+		MOVE_PATTERN = 1
+		TIMEOUT_LIMIT = 5
+		addTextView("請設定為滑動移動而不是螢幕搖桿")newRow()
+		addTextView("每隔120秒無戰鬥會振動, 每振動")addEditNumber("TIMEOUT_LIMIT", 5)
+		addTextView("次會中止script")newRow()
+		addTextView("移動方向:")newRow()
+		addRadioGroup("MOVE_PATTERN", 1)
+			addRadioButton("\\ 左上右下", 1)
+			addRadioButton("| 上下", 2)
+			addRadioButton("/ 左下右上", 3)
+			addRadioButton("- 左右", 4)
+			addRadioButton("O 繞圈", 5)
+			addRadioButton("R 隨機", 6) newRow()
+
+	    BATTLE_ABILITY = false
+	    BATTLE_DBS = false
+	    addCheckBox("BATTLE_ABILITY", "使用技能", false)addSpinner("BATTLE_DBS", DBScriptList, DBScriptList[1])newRow()
+    dialogShow("Auto Exploration")
+
+	self.movePattern = MOVE_PATTERN
+	self.timeOutLimit = TIMEOUT_LIMIT
+	--proSetScanInterval(2)
+	
+	
+	self.useAbility = BATTLE_ABILITY
+	if self.useAbility then
+		self.db = DesignedBattle()
+		self.db:decode(WORK_DIR .. BATTLE_DBS)
+	end
+
+	if BRIGHTNESS then
+		proSetBrightness(0)
+	end
 end
 
-function getAmtOfTroop(name)
-    amt = 0
-    troops = getInventoryTroops()
-    for i, v in troops do
-        if v == name then
-            amt = amt + 1
-        end
-    end
-    return amt
+function Explorer:run()
+	math.randomseed(os.time())
+	local LastBattle = Timer()
+	local timeout = 0
+	local battleCount = 0
+	local isTouchDown = false
+	local isAutoActivate = false
+	repeat
+		-- TODO If enter door
+		if (BattleIndicator:exists("Battle.png")) then
+			toast("In Battle")
+			if not self.useAbility and DesignedBattle.hasRepeatButton() then
+				isAutoActivate = DesignedBattle.clickAuto()
+			end
+
+			local battleRound = 1
+			repeat
+				if DEBUG then BattleIndicator:highlight(0.2) end
+				if BattleIndicator:exists("Battle.png") then
+					if self.useAbility and DesignedBattle.hasRepeatButton() then
+                        self.db:runScript(battleRound)
+						battleRound = battleRound + 1
+					end
+				else
+					if DEBUG then R23_0111:highlight(0.2) end
+					repeat until R23_0111:exists("ResultGil.png")
+					repeat
+						wait(0.5)
+						click(R23_0111:getLastMatch())
+					until not R23_0111:exists("ResultGil.png")
+					LastBattle:set()
+					
+					battleCount = battleCount + 1
+					local msg = "Battle count: "..battleCount
+					toast(msg)
+					setStopMessage(msg)
+					break
+				end
+			until false
+		end
+		if LastBattle:check() > 120 then
+			-- Notify user should move to next area, and reset timer for next 120s
+			vibratePattern()
+			LastBattle:set()
+			timeout = timeout + 1
+			print("Timeout"..timeout)
+			if timeout >= self.timeOutLimit then
+				break
+			end
+		end
+
+		self:move(self.movePattern)
+		FINISH = false
+	until FINISH
+	vibratePattern()
+	scriptExit("Auto exploration finish")
 end
 
-local data = TTD:WaitForModule("SharedSettings").TroopDatas
-local data2 = TTD:WaitForModule("SharedSettings").CrateDatas
-local datas = {}
-for k, l in next, data do
-    for i, v in pairs(l) do
-        if i == "DisplayName" then
-            datas[k] = v
-        end
-    end
-end
+function Explorer:move(pattern)
+	math.randomseed(os.time())
+	local inverse = math.random(1,20) % 2
 
-for k, l in next, data2 do
-    for i, v in pairs(l) do
-        if i == "DisplayName" then
-            datas[k] = v
-        end
-    end
-end
-
-function convertDisplayToId(display)
-    for i, v in datas do
-        if v == display then
-            return i
-        end
-    end
-end
-
-for i, v in toBuy do
-    local name = convertDisplayToId(i)
-    print(name, getAmtOfTroop(name))
-    if getAmtOfTroop(name) >= v[2] then
-        print(name, getAmtOfTroop(name), "more than", v[2])
-        toBuy[i] = nil
-    end
-end
-
-local tobuyN = 0
-
-for i, v in toBuy do
-    tobuyN = tobuyN + 1
-end
-
-if tobuyN == 0 then
-    writefile(game.Players.LocalPlayer.Name..".txt","Yummytool")
-    task.wait(1)
-    game:Shutdown()
-    coroutine.yield()
-end
-
-if game.PlaceId ~= 14682939953 then
-    if tobuyN > 0 then
-        game:GetService("TeleportService"):Teleport(14682939953, game:GetService("Players").LocalPlayer)
-    end
-else
-    if tobuyN == 0 then
-        game:GetService("TeleportService"):Teleport(13775256536, game:GetService("Players").LocalPlayer)
-    end
-end
-
-if game.PlaceId ~= 13775256536 then
-    local marketPos = CFrame.new(1438, 113, 2562)
-    local exitPos = CFrame.new(1437, 113, 2547)
-    local plr = game.Players.LocalPlayer
-    local root = plr.Character.HumanoidRootPart
-    local Gui = game:GetService("GuiService"); local Vim = game:GetService("VirtualInputManager")
-
-    function clickGui(guiName)
-        Gui.SelectedObject = guiName
-        wait(.1)
-        Vim:SendKeyEvent(true, 13, false, game)
-        wait(.1)
-        Vim:SendKeyEvent(false, 13, false, game)
-        wait(.1)
-        Gui.SelectedObject = nil
-        wait(.1)
-    end
-
-    function openMarketGui()
-        root.CFrame = marketPos
-        task.wait(0.5)
-        if not game:GetService("Players").LocalPlayer.PlayerGui.Lobby.MarketplaceFrame.Visible then
-            root.CFrame = exitPos
-            task.wait(0.5)
-            root.CFrame = marketPos
-        end
-    end
-
-    local excludeServers = true
-    local nextCursor = true
-    local deleteExcludedTimer = 1 -- this is in seconds
-
-
-    if not isfolder('server hop') then
-        makefolder('server hop')
-        writefile("server hop/tick.txt", tostring(tick()))
-    end
-
-    if not isfile("server hop/tick.txt") then
-        writefile("server hop/tick.txt", tostring(tick()))
-    else
-        local oldTick = tonumber(readfile("server hop/tick.txt"))
-        local timePassed = tick() - oldTick
-        print(timePassed)
-        if timePassed >= deleteExcludedTimer and excludeServers and isfile("server hop/excluded.json") then
-            delfile("server hop/excluded.json")
-            writefile("server hop/tick.txt", tostring(tick()))
-        end
-    end
-
-    local excluded = {}
-    if not isfile("server hop/excluded.json") then
-        if excludeServers then
-            local HttpService = game:GetService("HttpService")
-            local json = HttpService:JSONEncode(excluded)
-            writefile("server hop/excluded.json", json)
-        end
-    else
-        local HttpService = game:GetService("HttpService")
-        excluded = HttpService:JSONDecode(readfile('server hop/excluded.json'))
-    end
-
-
-    local function updateExcluded()
-        local HttpService = game:GetService("HttpService")
-        local json = HttpService:JSONEncode(excluded)
-        writefile("server hop/excluded.json", json)
-    end
-
-    --create the server hop function--
-    local function hop()
-        if not isfile("server hop/nextcursor.txt") then
-            writefile('server hop/nextcursor.txt', "")
-        end
-        local server
-        repeat
-            local reqString
-            if nextCursor then
-                reqString = "https://games.roblox.com/v1/games/" ..
-                    tostring(game.PlaceId) ..
-                    "/servers/Public?sortOrder=Desc&excludeFullGames=true&limit=100?cursor=" ..
-                    readfile('server hop/nextcursor.txt')
-            else
-                reqString = "https://games.roblox.com/v1/games/" ..
-                    tostring(game.PlaceId) .. "/servers/Public?sortOrder=Desc&excludeFullGames=true&limit=100"
-            end
-            local data = game:GetService("HttpService"):JSONDecode(game:HttpGet(reqString))
-            local currentServers = {}
-            if nextCursor then
-                writefile("server hop/nextcursor.txt", data.nextPageCursor)
-            end
-            for i, v in pairs(data.data) do
-                table.insert(currentServers, v)
-                if (v.playing < v.maxPlayers) and v.playing >= 20 then
-                    server = v.id
-                end
-            end
-            if server == nil then
-                for i, v in pairs(currentServers) do
-                    if (v.playing < v.maxPlayers) and v.playing >= 20 then
-                        server = v.id
-                    end
-                end
-            end
-            task.wait(1)
-        until server ~= nil
-        if excludeServers then
-            table.insert(excluded, server)
-            updateExcluded()
-        end
-        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, server, game.Players.LocalPlayer)
-    end
-    local function RealHop()
-        repeat
-            hop()
-            task.wait(3)
-        until 1 == true
-    end
-
-    print("open market gui")
-    openMarketGui()
-
-    for i, v in toBuy do
-        items = items + 1
-    end
-
-    task.spawn(function()
-        task.wait(20)
-        print("hop")
-        RealHop()
-    end)
-
-    task.spawn(function()
-        while game:GetService("Players").LocalPlayer.PlayerGui.Lobby.MarketplaceFrame.Visible do
-            task.wait()
-        end
-
-        RealHop()
-    end)
-
-    while task.wait() and items > 0 do
-        for i, v in game:GetService("Players").LocalPlayer.PlayerGui.Lobby.MarketplaceFrame.MarketplaceMain.MainFrame.BuyMenu.AllUnits:GetChildren() do
-            if not v:FindFirstChild("MainFrame") then continue end
-            if not v.MainFrame.UnitInfo.BestPrice.BestPrice.Visible then continue end
-
-            local price
-
-            suc, resp = pcall(function() price = v.MainFrame.UnitInfo.BestPrice.BestPrice.Text end)
-
-            if not suc then continue end
-
-            local name = v.MainFrame.UnitInfo.UnitName.Text
-            if not toBuy[name] then
-                continue
-            end
-
-
-            local huh = false
-            local suc, f = pcall(function()
-                if toBuy[name] and convertStringToNumber(price) > toBuy[name][1] then
-                    toBuy[name] = nil
-                end
-            end)
-
-            if not suc or huh then continue end
-
-
-            local buyButton = v.MainFrame.UnitInfo.Buttons.BuyUnit.BuyButton
-            clickGui(buyButton)
-            print "buying"
-
-            local confirmBtn
-            repeat
-                pcall(function()
-                    confirmBtn = game:GetService("Players").LocalPlayer.PlayerGui.Lobby.MarketplaceFrame.MarketplaceMain
-                        .MainFrame.ConfirmPopup.Options.Confirm.ConfirmButton
-                end)
-                wait()
-            until confirmBtn ~= nil
-            print("buying pt2")
-            clickGui(confirmBtn)
-
-            for i, v in toBuy do
-                if getAmtOfTroop(i) >= v[2] then
-                    toBuy[i] = nil
-                end
-            end
-
-            print "bought"
-        end
-        task.wait(1)
-        items = 0
-        for i, v in toBuy do
-            items = items + 1
-        end
-        print(items)
-    end
-    print "hop"
-    RealHop()
+	local directions = {}
+	local dirsCount = 0
+	if pattern == 1 then -- \
+		directions = {5, 7, 3, 7, 4, 7, 7, 7, 3, 7, 3, 5}
+	elseif pattern == 2 then -- |
+		directions = {5, 8, 2, 8, 2, 8, 8, 8, 2, 8, 2, 5}
+	elseif pattern == 3 then -- /
+		directions = {5, 9, 1, 9, 1, 9, 9, 9, 1, 9, 1, 5}
+	elseif pattern == 4 then -- -
+		directions = {5, 4, 6, 4, 6, 4, 4, 4, 6, 4, 6, 5}
+	elseif pattern == 5 then -- O
+		local startDir = math.random(1, 4) + inverse * 5
+		directions = {5, startDir, 4, 1, 2, 3, 6, 9, 8, 7, startDir, 5}
+	elseif pattern == 6 then -- Random
+		directions = {
+			5,
+			math.random(1, 4),
+			math.random(1, 4),
+			math.random(6, 9),
+			math.random(1, 4),
+			math.random(6, 9),
+			math.random(6, 9),
+			math.random(1, 4),
+			math.random(6, 9),
+			math.random(1, 4),
+			math.random(6, 9),
+			5
+		}
+	end
+	
+	if inverse == 1 then
+		invDirs = {}
+		for i,v in ipairs(directions) do
+		  invDirs[13 - i] = 10 - v
+		end
+		directions = invDirs
+	end
+	
+	local touchList = {}
+	local touchIdx = 1
+    touchList[touchIdx] = {action = "touchDown", target = DTABLE[directions[1]]}
+    touchIdx = touchIdx + 1
+    touchList[touchIdx] = {action = "wait", target = 0.2}
+    touchIdx = touchIdx + 1
+	for i = 2, 11 do
+		touchList[touchIdx] = {action = "touchMove", target = DTABLE[directions[i]]}
+	    touchIdx = touchIdx + 1
+		touchList[touchIdx] = {action = "wait", target = 0.4}
+	    touchIdx = touchIdx + 1
+	end
+    touchList[touchIdx] = {action = "touchUp", target = DTABLE[directions[12]]}
+    manualTouch(touchList)
 end
